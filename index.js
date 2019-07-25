3ebf14157fb29526fb309ce358d7f0587bc29ead
Garrett Halstein <garrett.halstein@emory.edu>
2019-07-25 17:21:24 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Your code here
+class Animal {
+	constructor(name, sex) {
+		this.name = name;
+		this.sex = sex;
+	}
+}
+
+class Cat extends Animal {
+  constructor() {
+  	super(name, sex);
+  }
+  speak() {
+  	return `${name} says meow!`
+  }
+}
+ 
+class Dog extends Animal {
+  constructor() {
+  	super(name, sex);
+  }
+  speak() {
+  	return `${name} says woof!`
+  }
+}
+ 
+class Bird extends Animal {
+  constructor() {
+  	super(name, sex);
+  }
+  speak() {
+  	return `${name} says squawk!`
+  }
+}
